19a730cc790f8f84403d00dadd4969a2ad73417f
israbarmar <barbotoi@yahoo.com>
2023-11-11 18:18:15 -0500

    update css

--- backstage.js
+++ backstage.js
@@ -101,7 +101,7 @@ function handleMouseWheel(event) {
 
   
      pink_a.forEach((p_link)=>{
-       if(indexBodyColor===0){
+       if(indexBodyColor!==1){
         p_link.style.color='white';
        } else {
         p_link.style.color='#FF608C';
